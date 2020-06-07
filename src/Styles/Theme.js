25706b3ca21158a91171ea004123d7316aaaf96a
Kyeongweon PARK <semipia@Kyeongweonui-MacBook-Air.local>
2020-06-07 14:54:40 +0900

    FrontEnd #7.2

---
+++ src/Styles/Theme.js
@@ -0,0 +1,19 @@
+const BOX_BORDER = "1px solid #dbdbdb";
+const BORDER_RADIUS = "2px";
+
+export default {
+    maxWidth: "935px",
+    bgColor: "#FAFAFA",
+    blackColor : "#262626",
+    darkGreyColor : "#8e8e8e",
+    lightGreyColor: "#c7c7c7",
+    redColor: "#E94856",
+    blueColor : "#3897f0",
+    darkBlueColor : "#00376b",
+    boxBorder: "1px solid #dbdbdb",
+    borderRadius: "2px",
+    whiteBox: `border:${BOX_BORDER};
+                border-radius:${BORDER_RADIUS};
+                background-color:white;
+    `
+};
